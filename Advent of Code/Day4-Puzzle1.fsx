f8d9553672f4bbb464d20d167f9aec26ce2cca67
blue <playbynumbers@gmail.com>
2015-12-20 22:38:18 +0000

    Solve Day 4, Puzzle 1

---
+++ Advent of Code/Day4-Puzzle1.fsx
@@ -0,0 +1,24 @@
+﻿open System.Security.Cryptography
+
+let md5 = MD5.Create()
+
+let secretKey = "bgvyzdsv"
+
+let mutable answer = 0
+
+let hash (answer:int) =
+    secretKey + answer.ToString() |>
+    System.Text.Encoding.ASCII.GetBytes |>
+    md5.ComputeHash |>
+    BitConverter.ToString
+
+let testHash (answer:int) =
+    if (hash answer:string).StartsWith("00-00-0") then
+        true
+    else
+        false
+
+while not (testHash answer) do
+    answer <- answer + 1
+
+answer
