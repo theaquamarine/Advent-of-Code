9a5b00a7a96a6b1e830d6c19e9c333048632cdcb
blue <playbynumbers@gmail.com>
2015-12-27 23:16:16 +0000

    Solve Day 12, Puzzle 1

---
+++ Advent of Code/Day12-Puzzle1.fsx
@@ -0,0 +1,10 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let input = __SOURCE_DIRECTORY__ + "\Day12-Input.txt" |>
+            File.ReadAllLines |>
+            String.concat("")
+
+Regex.Matches(input, "\-?[0-9]+") |>
+Seq.cast<Match> |>
+Seq.sumBy (fun m -> int m.Value)
